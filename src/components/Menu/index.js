749818c37cd45f4f65241744ffd971bf6f11f4ce
jackson-timoteo <jackson_siilva@hotmail.com>
2020-09-24 21:38:43 -0300

    video

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
-import './Menu.css'
+import './Menu.css';
 import Button from '../Button';
-//import ButtonLink from './components/ButtonLink';
+// import ButtonLink from './components/ButtonLink';
 
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="MobFlix" />
+      </Link>
 
-function Menu (){
-    return(
-        <nav className="Menu">
-            <Link to="/">
-                <img className="Logo" src={Logo} alt="MobFlix" />
-            </Link>
+      <Button as={Link} className="ButtonLink" to="cadastro/video">
+        Novo Vídeo
+      </Button>
+    </nav>
 
-            <Button as={Link} className="ButtonLink" to="cadastro/video">
-                Novo Vídeo
-            </Button>
-        </nav>
-
-    );
+  );
 }
 
 export default Menu;
