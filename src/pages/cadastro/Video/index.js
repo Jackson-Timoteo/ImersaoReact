ccd2eeef5b55f6863fefbb579bc8a5b029d99d02
jackson-timoteo <jackson_siilva@hotmail.com>
2020-10-01 23:15:51 -0300

    create form videos

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PagesDefault';
+import useForm from '../../../hocks/useForm';
+import FormField from '../../../components/FormField';
 
 function CadastroVideo() {
+  const { handleChange, values } = useForm({
+
+  });
+
   return (
     <PageDefault>
       <h1>Cadastro de Vídeos</h1>
+
+      <form>
+        <FormField
+          label="Titulo da Categoria"
+          name="titulo"
+          value={values.titulo}
+          onChange={handleChange}
+        />
+      </form>
 
       <Link to="/cadastro/categoria">
         Cadastro de novos Vídeos
